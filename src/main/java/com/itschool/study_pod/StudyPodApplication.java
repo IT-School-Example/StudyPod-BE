0eb82d435663bcea09a7573147b929e2d9b216e3
Lee Seungjong <lsjjong8@naver.com>
2025-04-17 09:40:01 +0900

    🔧 구성 파일 추가

---
+++ src/main/java/com/itschool/study_pod/StudyPodApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.study_pod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itschool/study_pod/StudyPodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.study_pod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
