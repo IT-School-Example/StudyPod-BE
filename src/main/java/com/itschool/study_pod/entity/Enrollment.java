23af682fb877448bd1e0e971c4b5a25dc6f9235c
ryu <yhd060210@gmail.com>
2025-04-22 10:19:24 +0900

    :sparkles: admins, users, enrollments 테이블 추가

---
+++ src/main/java/com/itschool/study_pod/entity/Enrollment.java
@@ -0,0 +1,43 @@
+package com.itschool.study_pod.entity;
+
+import com.itschool.study_pod.entity.base.BaseEntity;
+import com.itschool.study_pod.enumclass.EnrollmentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "enrollments")
+public class Enrollment extends BaseEntity {
+
+    @Id
+    @Column(name = "enrollment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appliedAt;
+
+    @Column(nullable = false)
+    private String introduce;
+
+    private LocalDateTime joinedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EnrollmentStatus status;
+
+    @Column(name = "study_group_id", nullable = false)
+    private Long studyGroupId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
