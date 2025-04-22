18c4565ec2b624cc74f25e91412f2a589a0e6b3f
윤하겸 <younha3710@naver.com>
2025-04-22 12:53:15 +0900

    📝study_groups, study_group_keywords,study_group_weekly_schedules 엔티티 설계

---
+++ src/main/java/com/itschool/study_pod/enumclass/DayOfWeek.java
@@ -0,0 +1,14 @@
+package com.itschool.study_pod.enumclass;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
