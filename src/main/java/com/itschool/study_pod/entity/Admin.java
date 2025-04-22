ea67a8391cab126565318347b09e6c29110500b5
ryu <yhd060210@gmail.com>
2025-04-22 10:19:24 +0900

    :sparkles: admins, users, enrollments 테이블 추가

---
+++ src/main/java/com/itschool/study_pod/entity/Admin.java
@@ -0,0 +1,32 @@
+package com.itschool.study_pod.entity;
+
+import com.itschool.study_pod.entity.base.BaseEntity;
+import com.itschool.study_pod.enumclass.AccountRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "admins")
+public class Admin extends BaseEntity {
+
+    @Id
+    @Column(name = "admin_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountRole role;
+}
