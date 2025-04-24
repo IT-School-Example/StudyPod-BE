85962d7ad160bd393b7e6c5d5a168002f5c6c23f
윤하겸 <younha3710@naver.com>
2025-04-24 14:54:50 +0900

    🎨StudyGroupRepository.java interface로 수정

--- src/main/java/com/itschool/study_pod/repository/StudyGroupRepository.java
+++ src/main/java/com/itschool/study_pod/repository/StudyGroupRepository.java
@@ -3,39 +3,12 @@ package com.itschool.study_pod.repository;
 import com.itschool.study_pod.entity.StudyGroup;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class StudyGroupRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public StudyGroup save(StudyGroup studyGroup) {
-        em.persist(studyGroup);
-        return studyGroup;
-    }
-
-    public void delete(StudyGroup studyGroup) {
-        em.remove(studyGroup);
-    }
-
-    public void deleteAll() {
-        em.createQuery("DELETE FROM StudyGroup").executeUpdate();
-    }
-
-    public List<StudyGroup> findAll() {
-        return em.createQuery("SELECT sg FROM StudyGroup sg", StudyGroup.class).getResultList();
-    }
-
-    public Optional<StudyGroup> findById(Long id) {
-        return Optional.ofNullable(em.find(StudyGroup.class, id));
-    }
-
-    public long count() {
-        return em.createQuery("SELECT COUNT(sg) FROM StudyGroup sg", Long.class).getSingleResult();
-    }
+public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
 }
