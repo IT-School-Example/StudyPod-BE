a2362368ce78f611cde4a2c00d692c06729b850c
Lee Seungjong <lsjjong8@naver.com>
2025-04-25 09:05:24 +0900

    🎨 StudyPodApplicationTests 수정

--- src/test/java/com/itschool/study_pod/StudyPodApplicationTests.java
+++ src/test/java/com/itschool/study_pod/StudyPodApplicationTests.java
@@ -1,9 +1,14 @@
 package com.itschool.study_pod;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("local")
 public class StudyPodApplicationTests {
 
 	@Test
