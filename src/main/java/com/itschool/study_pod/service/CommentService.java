0c108f62363275db33a71b9225a9723624b41f2b
박세빈 <psbspb7009@naver.com>
2025-04-25 15:20:58 +0900

    🔨 dto 및 service 추가

---
+++ src/main/java/com/itschool/study_pod/service/CommentService.java
@@ -0,0 +1,47 @@
+package com.itschool.study_pod.service;
+
+import com.itschool.study_pod.dto.request.Comment.CommentRequest;
+import com.itschool.study_pod.dto.response.CommentResponse;
+import com.itschool.study_pod.entity.Comment;
+import com.itschool.study_pod.repository.CommentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentResponse create(CommentRequest request) {
+        return commentRepository.save(Comment.of(request))
+                .response();
+    }
+
+    public CommentResponse read(Long id) {
+        return commentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException())
+                .response();
+    }
+
+    @Transactional
+    public CommentResponse update(Long id, CommentRequest commentRequest) {
+
+        Comment entity = commentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        entity.update(commentRequest);
+
+        return entity.response();
+    }
+
+    public void delete(long id) {
+        // commentRepository.deleteById(id);
+
+        Comment findEntity = commentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        commentRepository.delete(findEntity);
+    }
+}
