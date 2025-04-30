a2f07b5f83747d664c214253d50f686aaf01ea68
이유나 <jee1358@naver.com>
2025-04-30 16:56:48 +0900

    🔨 InterestedStudy.java 외 파일3개 수정

---
+++ src/main/java/com/itschool/study_pod/entity/InterestedStudy.java
@@ -0,0 +1,27 @@
+package com.itschool.study_pod.entity;
+
+import com.itschool.study_pod.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+// 컬럼 작성 보류
+public class InterestedStudy extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interested_study_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_group_id", nullable = false)
+    private StudyGroup studyGroup;
+}
+
