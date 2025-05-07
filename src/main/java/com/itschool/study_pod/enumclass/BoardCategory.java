d577e10a2420186d2dfefce18e43f76340628295
박세빈 <psbspb7009@naver.com>
2025-05-07 15:40:57 +0900

    🔨BoardCategory 수정

--- src/main/java/com/itschool/study_pod/enumclass/BoardCategory.java
+++ src/main/java/com/itschool/study_pod/enumclass/BoardCategory.java
@@ -5,9 +5,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum BoardCategory {
     NOTICE("공지사항"),
-    QNA("자주하는 질문"),
     FREE("자유 게시판"),
-    STUDY("스터디 내 게시판");
+    STUDY("스터디 내 공지사항");
 
     private final String description;
 }
