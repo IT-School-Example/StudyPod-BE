e4829cb795236ca5592db8f89cfee167a8dd2a70
ryu <yhd060210@gmail.com>
2025-05-12 13:08:36 +0900

    🔨 Introduce Service Controller Repository 추가 및 수정

---
+++ src/main/java/com/itschool/study_pod/controller/api/IntroduceController.java
@@ -0,0 +1,24 @@
+package com.itschool.study_pod.controller.api;
+
+import com.itschool.study_pod.controller.base.CrudController;
+import com.itschool.study_pod.dto.request.IntroduceRequest;
+import com.itschool.study_pod.dto.response.IntroduceResponse;
+import com.itschool.study_pod.entity.Introduce;
+import com.itschool.study_pod.service.IntroduceService;
+import com.itschool.study_pod.service.base.CrudService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/introduce")
+public class IntroduceController extends CrudController<IntroduceRequest, IntroduceResponse, Introduce> {
+
+    private final IntroduceService introduceService;
+
+    @Override
+    protected CrudService<IntroduceRequest, IntroduceResponse, Introduce> getBaseService() {
+        return introduceService;
+    }
+}
