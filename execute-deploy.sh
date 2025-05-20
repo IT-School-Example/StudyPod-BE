5db2821b0278f4337f3044b55c50d1295b3963de
Lee Seungjong <lsjjong8@naver.com>
2025-05-20 14:33:16 +0900

    🔨 배포 후 실행 스크립트 수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo -u ec2-user /home/ec2-user/app/deploy/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+sudo -u ec2-user /home/ec2-user/app/deploy/deploy.sh > /home/ec2-user/app/deploy/deploy.log 2>&1 < /dev/null &
+#sudo -u ec2-user /home/ec2-user/app/deploy/deploy.sh > /dev/null 2> /dev/null < /dev/null &
